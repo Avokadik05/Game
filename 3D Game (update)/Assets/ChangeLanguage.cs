8b068f7d69077a821b0d2f68d132477147e284f6
Avokadik05 <undo7515@gmail.com>
2022-05-29 11:34:47 +0300

    fix 29/05/22
    Починил меню настроек, пока что работает, поэтому делаю бэкап

---
+++ 3D Game (update)/Assets/ChangeLanguage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeLanguage : MonoBehaviour
+{
+    [SerializeField]
+    private LangsList lang;
+
+    private void Start()
+    {
+        lang.Load();
+    }
+
+    public void ChangeLang(Dropdown drop)
+    {
+        LangsList.SetLanguage(drop.value, true);
+    }
+}
