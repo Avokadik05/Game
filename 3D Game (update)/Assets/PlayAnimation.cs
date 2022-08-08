7e667f65f374f0d9028792f56e4cbb95dc899963
Avokadik05 <undo7515@gmail.com>
2022-08-08 17:17:58 +0300

    update 08/08/22
    TestLevel:
    - Добавил разные шейдеры (дон юз тзис, плиз)
    - Работаю над "сайнити"
    - !Добавил инверсивную кинематику!

---
+++ 3D Game (update)/Assets/PlayAnimation.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class PlayAnimation : MonoBehaviour
+{
+    public Rig mob_rig;
+    public float _weightSpeed = 3;
+
+    int attack_ID;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) attack_ID = 1;
+        else if(Input.GetKeyDown(KeyCode.T)) attack_ID = 2;
+
+        if(attack_ID == 1)
+        {
+            mob_rig.weight = Mathf.MoveTowards(mob_rig.weight, 1, _weightSpeed * Time.deltaTime);
+            if (mob_rig.weight == 1) attack_ID = 0;
+        }
+        else if(attack_ID == 2)
+        {
+            mob_rig.weight = Mathf.MoveTowards(mob_rig.weight, 0, _weightSpeed * Time.deltaTime);
+            if (mob_rig.weight == 0) attack_ID = 0;
+        }
+    }
+}
